4b9e20196aab39ba58a55439de67c95161148d6a
MyriamCounilh <myriam.counilh@aticdnet.com>
2022-03-08 12:59:20 +0100

    Problème de l'immatriculation unique résolu

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -71,8 +71,8 @@ public class TicketDAO {
         }finally {
             dataBaseConfig.closeConnection(con);
             dataBaseConfig.closePreparedStatement(ps);
-            return ticket;
         }
+        return ticket;
     }
 
     public boolean updateTicket(Ticket ticket) {
