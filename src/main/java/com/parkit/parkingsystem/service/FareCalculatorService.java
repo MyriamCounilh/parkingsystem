b6f97a02dd371eb83d545df980d37eb569b82974
MyriamCounilh <myriam.counilh@aticdnet.com>
2022-03-16 10:49:05 +0100

    Ajout d'une méthode pour parking gratuit 30 min

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,15 +5,10 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
-
+    public long durationInMinute(Ticket ticket) {
         long inHour = ticket.getInTime().getTime();
         long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
         long durationInMinute = (outHour - inHour) / (60 * 1000);
 
         /**
@@ -25,13 +20,26 @@ public class FareCalculatorService {
             durationInMinute = 0;
         }
 
+        return durationInMinute;
+    }
+
+    public void calculateFare(Ticket ticket){
+
+        long duration = durationInMinute(ticket);
+
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
+            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+        }
+
+        //TODO: Some tests are failing here. Need to check if this logic is correct
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice( ( durationInMinute * Fare.CAR_RATE_PER_HOUR ) / 60 );
+                ticket.setPrice( ( duration * Fare.CAR_RATE_PER_HOUR ) / 60 );
                 break;
             }
             case BIKE: {
-                ticket.setPrice( ( durationInMinute * Fare.BIKE_RATE_PER_HOUR ) / 60 );
+                ticket.setPrice( ( duration * Fare.BIKE_RATE_PER_HOUR ) / 60 );
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
